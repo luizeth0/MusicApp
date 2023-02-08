07e2928f07cb8bc1adc0bb1a7532686e6af4c73c
luizeth0 <luis.hemi94@gmail.com>
2023-02-08 12:22:06 -0500

    Create project, gradle edit with dependencies and the feature binding and layout design

---
+++ app/src/main/java/com/example/musicapp/FullscreenActivity.kt
@@ -0,0 +1,28 @@
+package com.example.musicapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.musicapp.databinding.ActivityFullscreenBinding
+
+@Suppress("DEPRECATION")
+class FullscreenActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityFullscreenBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            val intent = Intent(this@FullscreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+
+}
